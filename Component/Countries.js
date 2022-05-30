343eaef72596503a9ed377123c2fa1190ebcf24a
SabbirSami <smd71430@gmail.com>
2022-05-30 12:15:05 +0600

    load countries api and showing in app

--- Component/Countries.js
+++ Component/Countries.js
@@ -1,10 +1,16 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export default function Countries() {
+    const [countries, setCountries] = useState([]);
+    useEffect(() => {
+        fetch("https://restcountries.com/v3.1/all")
+            .then((res) => res.json())
+            .then((data) => setCountries(data));
+    }, []);
     return (
         <View>
-            <Text>Countries</Text>
+            <Text>Countries {countries.length}</Text>
         </View>
     );
 }
